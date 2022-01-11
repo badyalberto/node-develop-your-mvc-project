a9ae55d9a9a4b3c1f75ca7836612e092c558221e
Alberto Chica <badyalberto@gmail.com>
2022-01-11 09:03:58 +0100

    fix commits

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -8,17 +8,11 @@ const UserRouter = Router();
 UserRouter.get("/users", UserController.getAllUsers);
 
 UserRouter.get("/users/:userId", UserController.getUserById);
-
 UserRouter.post("/users", UserController.createUser);
-
 UserRouter.post("/sign-up", UserController.signUp);
-
 UserRouter.post("/sign-in", AuthController.signIn);
-
 UserRouter.post("/sign-out", AuthController.signOut);
-
 UserRouter.patch("/users/:userId", UserController.updateUser);
-
 UserRouter.delete("/users/:userId", UserController.deleteUser);
 
 module.exports = UserRouter;
